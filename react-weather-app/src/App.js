a129f0e568ed59658d2cd06a3c88031d2cb57846
rafidmahin20 <submanmahin@gmail.com>
2023-06-22 23:24:40 +0600

    search component created

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import TopButtons from './Components/TopButtons';
+import Search from './Components/Search';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons/>
+      <Search/>
     </div>
   );
 }
---
+++ react-weather-app/src/Components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+
+function Search() {
+  return (
+    <div className="flex flex-row justify-center my-6">
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4"></div>
+    </div>
+  )
+}
+
+export default Search
