81e743f1ae15c3ce246f7870934cf27d054b8f71
rafidmahin20 <submanmahin@gmail.com>
2023-06-23 11:48:10 +0600

    added the search field

--- react-weather-app/src/Components/Search.js
+++ react-weather-app/src/Components/Search.js
@@ -4,7 +4,18 @@ import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
 function Search() {
   return (
     <div className="flex flex-row justify-center my-6">
-        <div className="flex flex-row w-3/4 items-center justify-center space-x-4"></div>
+        <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+          <input 
+            type='text'
+            placeholder="search for city..."
+            className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"/>
+            <UilSearch
+            size={25}
+            className="text-white cursor-pointer transition ease-out hover:scale-125"/>
+            <UilLocationPoint
+            size={25}
+            className="text-white cursor-pointer transition ease-out hover:scale-125"/>
+        </div>
     </div>
   )
 }
