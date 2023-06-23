4371baaffaf58698d041956b63b5bb1b4dd8ad94
rafidmahin20 <submanmahin@gmail.com>
2023-06-23 12:18:09 +0600

    editing the time and location component

--- react-weather-app/src/Components/TimeAndLocation.js
+++ react-weather-app/src/Components/TimeAndLocation.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 function TimeAndLocation() {
   return (
-    <div className="flex flex-row items-center justify-center my-6">
+    <div>
+        <div className="flex flex-row items-center justify-center my-6">
+            <p className="text-white text-xl font-extralight">
+                Tuesday, 23 June 2023 | Local Time: 12.36 PM
+            </p>
+        </div>
 
-        
-
-
+        <div className="flex flex-row items-center justify-center my-6">
+            <p className="text-white text-xl font-extralight">Berlin, DE</p>
+        </div>
     </div>
   )
 }
