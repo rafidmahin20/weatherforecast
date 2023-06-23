bee8394639d469c9dd4314585b71e9f599e81636
rafidmahin20 <submanmahin@gmail.com>
2023-06-23 12:10:33 +0600

    creating new component time and location

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react';
 import TopButtons from './Components/TopButtons';
 import Search from './Components/Search';
+import TimeAndLocation from './Components/TimeAndLocation';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons/>
       <Search/>
+
+      <TimeAndLocation/>
     </div>
   );
 }
---
+++ react-weather-app/src/Components/TimeAndLocation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div className="flex flex-row items-center justify-center my-6">
+
+        
+
+
+    </div>
+  )
+}
+
+export default TimeAndLocation
